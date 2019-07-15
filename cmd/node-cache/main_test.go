d07df76aa7aa6aef6d49f73f1149709b8cc2e6f7
Yann Hamon <yann.hamon@contentful.com>
2019-07-15 10:18:07 +0200

    add test for ensureIptablesrulesPresent

--- cmd/node-cache/main_test.go
+++ cmd/node-cache/main_test.go
@@ -33,10 +33,19 @@ func (ipt *mockIptablesEnsurer) EnsureRule(position utiliptables.RulePosition, t
 	return false, nil
 }
 
+func (ipt *mockIptablesEnsurer) invocations() int {
+	return ipt.nCalls
+}
+
+func (ipt *mockIptablesEnsurer) reset() {
+	ipt.nCalls = 0
+}
+
 func TestEnsureIptablesRulesPresent(t *testing.T) {
 	type testCase struct {
 		name           string
 		rules          []iptablesRule
+		ruleEnsurer    mockIptablesEnsurer
 		expectedRes    error
 		expectedNCalls int
 	}
@@ -44,16 +53,18 @@ func TestEnsureIptablesRulesPresent(t *testing.T) {
 	ipt := mockIptablesEnsurer{}
 
 	testCases := []testCase{
-		{name: "one listen interface, all rules", rules: iptablesRules("192.168.1.100", "80"), expectedRes: nil, expectedNCalls: 8},
+		{name: "one ip, all rules", rules: iptablesRules("192.168.1.100", "80"), ruleEnsurer: ipt, expectedRes: nil, expectedNCalls: 8},
+		{name: "two ips, all rules", rules: iptablesRules("192.168.1.100,192.168.1.101", "80"), ruleEnsurer: ipt, expectedRes: nil, expectedNCalls: 16},
 	}
 
 	for _, testCase := range testCases {
-		err := ensureIptablesRulesPresent(testCase.rules, &ipt)
+		testCase.ruleEnsurer.reset()
+		err := ensureIptablesRulesPresent(testCase.rules, &testCase.ruleEnsurer)
 		if err != testCase.expectedRes {
-			t.Errorf("test %s failed, expected %s, got %s", testCase.name, testCase.expectedRes, err)
+			t.Errorf("test '%s' failed, expected %s, got %s", testCase.name, testCase.expectedRes, err)
 		}
-		if ipt.nCalls != testCase.expectedNCalls {
-			t.Errorf("test %s failed, expected %d calls, got %d", testCase.name, testCase.expectedNCalls, ipt.nCalls)
+		if testCase.ruleEnsurer.invocations() != testCase.expectedNCalls {
+			t.Errorf("test '%s' failed, expected %d calls, got %d", testCase.name, testCase.expectedNCalls, testCase.ruleEnsurer.invocations())
 		}
 	}
 }
